8b1c39d96ec3e51dc26f97e4d5d4b4de70e0ffcb
Sundar Thulasiraman <sundar50000@gmail.com>
2025-07-21 03:20:00 +0000

    delete process.env file

--- next.config.ts
+++ next.config.ts
@@ -19,18 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    // Expose server-side variables to the client
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    WHATSAPP_API_URL: process.env.WHATSAPP_API_URL,
-    WHATSAPP_API_TOKEN: process.env.WHATSAPP_API_TOKEN,
-    WHATSAPP_SENDER_MOBILE: process.env.WHATSAPP_SENDER_MOBILE,
-  }
 };
 
 export default nextConfig;
