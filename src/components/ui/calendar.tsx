249748ab1e8e502cd3376a6561251003f2af3eb5
Sundar Thulasiraman <sundar50000@gmail.com>
2025-07-23 22:23:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button id="date"

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,7 +25,6 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
-        caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -59,9 +58,6 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      captionLayout="dropdown-buttons"
-      fromYear={1990}
-      toYear={2030}
       {...props}
     />
   )
