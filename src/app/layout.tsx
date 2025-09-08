694113423b1c711b42406e2933f17ce2cd5a518d
Sundar Thulasiraman <sundar50000@gmail.com>
2025-09-08 18:58:44 +0000

    please convert the below code and add it in the header file
    <!--Start o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/66bf2589d0c55f1921267528/1j4lal8l5';
+            s1.src='https://embed.tawk.to/68bf2589d0c55f1921267528/1j4lal8l5';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
