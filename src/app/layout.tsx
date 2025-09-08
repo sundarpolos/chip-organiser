a184644938fce6c45344d1e686c891631d77d611
Sundar Thulasiraman <sundar50000@gmail.com>
2025-09-08 18:53:35 +0000

    Add the below coding in header file
    <!--Start of Tawk.to Script-->
    <scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans" });
 
@@ -29,6 +30,19 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <Script id="tawk-to-script" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66bf2589d0c55f1921267528/1j4lal8l5';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
